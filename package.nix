429069a6e456fb5d80f766f98ef2af14401cfa78
Jeppe Wolff Johansen <jepper123411@proton.me>
2026-03-20 14:53:16 +0100

    Updated package.nix

--- package.nix
+++ package.nix
@@ -12,11 +12,22 @@
       ];
     in
     {
-      packages.default = pkgs.buildGoModule {
+      packages.dev = pkgs.buildGoModule {
         pname = "prot";
-        inherit version ldflags;
+        inherit version;
         src = ./.;
         vendorHash = "sha256-1xQSQTUZUzykz8YXVnIp5bImU9cJCODiA3cWeb852w0=";
       };
+			packages.default = pkgs.buildGoModule {
+				pname = "prot";
+				inherit version ldflags;
+				src = pkgs.fetchFromGitHub {
+					owner = "z3co";
+					repo = "protv2";
+					tag = "v1.0.1";
+					hash = "sha256-YSxmIzbgAyFO9kVlLA6wUt1vwO2pIiaN970rGp+8/cw=";
+				};
+				vendorHash = "sha256-1xQSQTUZUzykz8YXVnIp5bImU9cJCODiA3cWeb852w0=A";
+			};
     };
 }
