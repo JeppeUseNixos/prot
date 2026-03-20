4ab3226d1d03850e96ed679bfd21b28da0a1e767
Jeppe Wolff Johansen <jepper123411@proton.me>
2026-03-20 14:58:16 +0100

    Added prot to devshell

--- flake.nix
+++ flake.nix
@@ -1,29 +1,46 @@
 {
-  description = "Description for the project";
+	description = "Description for the project";
 
-  inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+	inputs = {
+		flake-parts.url = "github:hercules-ci/flake-parts";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	};
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
+	outputs =
+		inputs@{ flake-parts, ... }:
+		flake-parts.lib.mkFlake { inherit inputs; } {
+			imports = [
 				./package.nix
-      ];
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = {pkgs, self', ... }: {
-				devShells.default = pkgs.mkShell {
-					name = "go";
-					packages = with pkgs; [
-						go just zsh sqlc cobra-cli zip gopls gofumpt
-					];
-					shellHook = ''
-						export APP_VERSION=${self'.packages.default.version}
-						export SHELL=${pkgs.zsh}/bin/zsh
-						exec $SHELL
+			];
+			systems = [
+				"x86_64-linux"
+				"aarch64-linux"
+				"aarch64-darwin"
+				"x86_64-darwin"
+			];
+			perSystem =
+				{ pkgs, self', ... }:
+				{
+					devShells.default = pkgs.mkShell {
+						name = "go";
+						packages = with pkgs; [
+							go
+							just
+							zsh
+							sqlc
+							cobra-cli
+							zip
+							gopls
+							gofumpt
+							self'.packages.default
+							nixfmt
+						];
+						shellHook = ''
+													export APP_VERSION=${self'.packages.default.version}
+													export SHELL=${pkgs.zsh}/bin/zsh
+													exec $SHELL
 						'';
+					};
 				};
-      };
-    };
+		};
 }
